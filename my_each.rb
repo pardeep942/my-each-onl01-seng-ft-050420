c42a602ce95dc29534a5e0a70963810f7ee85bd9
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 05:53:54 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,9 @@
 def my_each # put argument(s) here
- collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
+ def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
 end
